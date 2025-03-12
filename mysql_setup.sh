66c209086bf3603ac40a5d018e3aaa04f39541df
Muhammad Nour Zibdie <nour@zibdie.com>
2025-03-12 06:58:48 -0400

    Dont restrict on IP

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Start MySQL with binding to all interfaces (0.0.0.0)
 mysqld_safe \
+--bind-address=0.0.0.0 \
 --max_allowed_packet=1G \
 --net_buffer_length=1000000 \
 --net_read_timeout=3600 \
@@ -13,9 +15,12 @@ mysqld_safe \
 
 sleep 5
 
+# Create users that can connect from any host
 mysql -u root -e "
-CREATE USER IF NOT EXISTS 'root'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'root';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 SET GLOBAL max_allowed_packet=1024*1024*1024;
 SET GLOBAL net_buffer_length=1000000;
